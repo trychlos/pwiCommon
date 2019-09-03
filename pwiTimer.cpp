b72aaa062d948490a873a864b6bd64e9c36e350c
Pierre Wieser <pwieser@trychlos.org>
2019-09-04 00:32:33 +0200

    pwiTimer: initial commit

---
+++ pwiTimer.cpp
@@ -0,0 +1,293 @@
+#include "pwiTimer.h"
+#include "untilNow.h"
+
+/*
+ * pwi 2017- 5-20 v3 add getRemaining() method
+ * pwi 2019- 5-25 v4 remove start() with argument method
+ *                   set() is renamed setup()
+ * pwi 2019- 5-27 v5 renamed to pwiTimer
+ * pwi 2019- 6- 3 v6 improve resetting the timer delay
+ */
+
+// uncomment to debugging this file
+//#define TIMER_DEBUG
+
+// max count of allocatable pwiTimer's
+#define PWITIMER_MAX 15
+
+static pwiTimer *st_timers[PWITIMER_MAX];
+static uint8_t   st_count = 0;
+
+/**
+ * pwiTimer::pwiTimer:
+ * 
+ * Constructor.
+ */
+pwiTimer::pwiTimer( void )
+{
+    this->delay_ms = 0;
+    this->once = true;
+    this->cb = NULL;
+    this->user_data = NULL;
+    this->start_ms = 0;
+    this->label = NULL;
+    this->debug = true;
+
+    if( st_count == 0 ){
+        memset( st_timers, '\0', sizeof( st_timers ));
+    }
+    if( st_count < PWITIMER_MAX ){
+        st_timers[st_count++] = this;
+    }
+}
+
+/**
+ * pwiTimer::getRemaining:
+ *
+ * Returns: the remaining count of ms.
+ */
+unsigned long pwiTimer::getRemaining( void )
+{
+    unsigned long duration = untilNow( millis(), this->start_ms );
+    unsigned long remaining = untilNow( this->delay_ms, duration );
+#ifdef TIMER_DEBUG
+    Serial.print( F( "[pwiTimer::getRemaining] delay_ms=" ));
+    Serial.print( this->delay_ms );
+    Serial.print( F( ", start_ms=" ));
+    Serial.print( this->start_ms );
+    Serial.print( F( ", now=" ));
+    Serial.print( millis());
+    Serial.print( F( ", duration=" ));
+    Serial.print( duration );
+    Serial.print( F( ", remaining=" ));
+    Serial.print( remaining );
+#endif
+    return( remaining );
+}
+
+/**
+ * pwiTimer::isStarted:
+ * 
+ * Returns: %TRUE if the timer is started.
+ */
+bool pwiTimer::isStarted( void )
+{
+    return( this->start_ms > 0 );
+}
+
+/**
+ * pwiTimer::restart:
+ * 
+ * Start the timer,
+ * or restart it before its expiration thus reconducting a new delay.
+ */
+void pwiTimer::restart( void )
+{
+    this->objStart( true );
+}
+
+/**
+ * pwiTimer::setDelay:
+ * @delay_ms: the duration of the timer.
+ *
+ * Remarks :
+ * 1. if the new delay is zero, the timer is stopped.
+ * 2. when decreasing the delay, the user expects the next callback to be at most
+ *    at this new delay...
+ * 
+ * Change the configured delay.
+ */
+void pwiTimer::setDelay( unsigned long delay_ms )
+{
+	unsigned long remaining = this->getRemaining();
+    this->delay_ms = delay_ms;
+
+	if( delay_ms == 0 ){
+		this->stop();
+	}
+
+	if( remaining > delay_ms ){
+		this->restart();
+	}
+}
+
+/**
+ * pwiTimer::setup:
+ * @label: [allow-none]: a label to identify or qualify the timer;
+ *  as we do not copy the string, it must stay valid during the life
+ *  of the object.
+ * @delay_ms: the duration of the timer; must be greater than zero.
+ * @once: whether the @cb callback must be called only once, or regularly.
+ * @cb: [allow-none]: the callback.
+ * @user_data: [allow-none]: the user data to be passed to the callback.
+ * @debug: [allow-none]: whether the pwiTimer behavior should be debugged.
+ * 
+ * Initialize the timer.
+ * After having been initialized, the timer may be started.
+ * If set, the @cb callback will be called at the expiration of the @delay_ms.
+ * 
+ * If @once is %TRUE, the timer is then disabled, and will stay inactive
+ * until started another time.
+ * 
+ * If @once is %FALSE, the @cb callback will be called regularly each
+ * @delay_ms.
+ */
+void pwiTimer::setup( const char *label, unsigned long delay_ms, bool once, pwiTimerCb cb, void *user_data, bool debug )
+{
+    if( strlen( label )){
+        this->label = label;
+    }
+    this->delay_ms = delay_ms;
+    this->once = once;
+    this->cb = cb;
+    this->user_data = user_data;
+    this->debug = debug;
+}
+
+/**
+ * pwiTimer::start:
+ * 
+ * Start a timer.
+ * 
+ * The pre-set delay must be greater than zero.
+ */
+void pwiTimer::start( void )
+{
+    this->objStart( false );
+}
+
+/**
+ * pwiTimer::stop:
+ *
+ * Stop a timer.
+ */
+void pwiTimer::stop( void )
+{
+    this->start_ms = 0;
+}
+
+/**
+ * pwiTimer::dump:
+ * 
+ * Dump.
+ * 
+ * Public Static
+ */
+void pwiTimer::Dump( void )
+{
+#ifdef TIMER_DEBUG
+    Serial.print( F( "[pwiTimer::dump]: st_count=" ));
+    Serial.print( st_count );
+    Serial.print( "/" );
+    Serial.println( PWITIMER_MAX );
+    for( uint8_t i=0 ; i<st_count ; ++i ){
+        st_timers[i]->objDump( i );
+    }
+#endif
+}
+
+/**
+ * pwiTimer::runLoop:
+ * 
+ * This function is meant to be called from the main loop.
+ * 
+ * Public Static
+ */
+void pwiTimer::Loop( void )
+{
+    for( uint8_t i=0 ; i<st_count ; ++i ){
+        st_timers[i]->objLoop();
+    }
+}
+
+/**
+ * pwiTimer::objDump:
+ * 
+ * Dump the object.
+ * 
+ * Private
+ */
+void pwiTimer::objDump( uint8_t idx )
+{
+#ifdef TIMER_DEBUG
+    Serial.print( F( "[pwiTimer::objDump] idx=" ));
+    Serial.print( idx );
+    if( strlen( this->label )){
+        Serial.print( F( ", label=" ));
+        Serial.print( this->label );
+    }
+    Serial.print( F( ", delay_ms=" ));
+    Serial.print( this->delay_ms );
+    Serial.print( F( ", once=" ));
+    Serial.print( once ? "True":"False" );
+    Serial.print( F( ", cb=" ));
+    Serial.print(( int ) cb );
+    Serial.print( F( ", user_data=" ));
+    Serial.print(( int ) user_data );
+    Serial.print( F( ", debug=" ));
+    Serial.println( debug ? "True":"False" );
+#endif
+}
+
+/**
+ * pwiTimer::objLoop:
+ * 
+ * If the timer is started, and has expired, then call the callback. 
+ * Stop the timer if set for running once.
+ * 
+ * Private
+ */
+void pwiTimer::objLoop( void )
+{
+    if( this->start_ms > 0 ){
+        unsigned long duration = untilNow( millis(), this->start_ms );
+        if( duration > this->delay_ms ){
+#ifdef TIMER_DEBUG
+            if( this->debug ){
+                Serial.print( F( "[pwiTimer::objLoop] " ));
+                if( strlen( this->label )){
+                    Serial.print( F( "label=" ));
+                    Serial.print( this->label );
+                    Serial.print( ", " );
+                }
+                Serial.print( F( "delay_ms=" ));
+                Serial.print( this->delay_ms );
+                Serial.print( F( ", start_ms=" ));
+                Serial.print( this->start_ms );
+                Serial.print( F( ", duration=" ));
+                Serial.println( duration );
+            }
+#endif
+            if( this->once ){
+                this->start_ms = 0;
+            } else {
+                this->start_ms = millis();
+            }
+            if( this->cb ){
+                this->cb( this->user_data );
+            }
+        }
+    }
+}
+
+/**
+ * pwiTimer::objStart:
+ * @restart: whether we accept to re-start an already started timer.
+ * 
+ * Start or restart the timer.
+ * 
+ * Private
+ */
+void pwiTimer::objStart( bool restart )
+{
+    if( this->delay_ms > 0 ){
+        if( this->start_ms > 0 && !restart ){
+            Serial.println( F( "[pwiTimer::objStart] ERROR: timer already started" ));
+        } else {
+            this->start_ms = millis();
+        }
+    } else {
+        Serial.println( F( "[pwiTimer::objStart] ERROR: unable to start the timer while delay is not set" ));
+    }
+}
+
---
+++ pwiTimer.h
@@ -0,0 +1,80 @@
+#ifndef __PWI_TIMER_H__
+#define __PWI_TIMER_H__
+
+#include <Arduino.h>
+
+/*
+ * This is a very simple timer which let us trigger a function at the end
+ * of a predefined delay.
+ * Contrarily to other standard real timers, this one does not rely on any
+ * of the internal Arduino timers, nor on any interrupts. Instead this one
+ * relies on being called repeatidly on each loop.
+ *
+ * Do not expect a great precision here. In best case, it will be about 1-2 ms.
+ * The precision may decrease until 10-20 ms if the main loop is too long.
+ *
+ * Synopsys:
+ *
+ * Define a new timer:
+ *   pwiTimer timer.setup( delay_ms, once, cb, user_data );
+ *
+ *   It is caracterized by the delay, the callback and the user_data.
+ *
+ * Start a defined timer:
+ *   timer.start();
+ *
+ *   The predefined timer is started.
+ *   This timer is not reentrant: once it has been started, it cannot
+ *   be reused before having been stopped before.
+ *   At end of the predefined delay, the callback will be called with
+ *   the passed-in user data. At this time, the timer is automatically
+ *   disabled, and should be re-started for another use.
+ *   This is a one time call timer.
+ *
+ * Once more time: this simplissim timer relies on being repeatedly called by
+ * the main loop.
+ *
+ * pwi 2017- 5-20 v3 add getRemaining() method
+ * pwi 2019- 5-25 v4 remove start() with argument method
+ *                   set() is renamed setup()
+ * pwi 2019- 5-27 v5 renamed to pwiTimer
+ * pwi 2019- 6- 3 v6 improve resetting the timer delay
+ */
+
+/* The prototype for the timer callback function to be provided by the caller.
+   This function receives the 'user_data' parameter provided at setup() time.
+   No return value is expected.
+ */
+typedef void ( *pwiTimerCb )( void * );
+
+class pwiTimer {
+    public:
+                      pwiTimer( void );
+        unsigned long getRemaining();
+        bool          isStarted();
+        void          restart( void );
+        void          setDelay( unsigned long delay_ms );
+        void          setup( const char *label, unsigned long delay_ms, bool once=false, pwiTimerCb cb=NULL, void *user_data=NULL, bool debug=true );
+        void          start( void );
+        void          stop( void );
+
+        static void   Dump();
+        static void   Loop();
+
+    private:
+        const char   *label;
+        unsigned long delay_ms;
+        bool          once;
+        pwiTimerCb    cb;
+        void         *user_data;
+        bool          debug;
+
+        unsigned long start_ms;
+
+        void          objDump( uint8_t idx );
+        void          objLoop( void );
+        void          objStart( bool restart );
+};
+
+#endif // __PWI_TIMER_H__
+
