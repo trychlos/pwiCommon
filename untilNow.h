de4219b0858d0ae04022b750a06849d571424ca4
Pierre Wieser <pwieser@trychlos.org>
2019-09-05 21:38:14 +0200

    untilNow: initial commit

---
+++ untilNow.h
@@ -0,0 +1,14 @@
+#ifndef __UNTIL_NOW_H__
+#define __UNTIL_NOW_H__
+
+/*
+ * pwi 2017- 5- 2 v1 creation
+ * pwi 2019- 5-27 v2 renamed to untilNow
+ */
+
+#include <Arduino.h>
+
+unsigned long untilNow( unsigned long now_ms, unsigned long start_ms );
+
+#endif // __UNTIL_NOW_H__
+
